350dd668b689538b48c47d661f0cfc696915a314
Patrick R <hi@patrickrein.de>
2023-06-23 09:09:09 +0200

    first commit

---
+++ so-add-badges.js
@@ -0,0 +1,13 @@
+
+//document.addEventListener("DOMContentLoaded", function(){
+	const chatGPTReleaseDate = new Date("2022-11-30");
+	for(element of document.querySelectorAll("span.relativetime")) { 
+		let elementDate = new Date(element.getAttribute("title"));
+		if (elementDate < chatGPTReleaseDate) {
+			element.classList.add("prechatgpt");
+		} else {
+			element.classList.add("postchatgpt");
+		}
+	}
+	console.log("foo");
+//});
