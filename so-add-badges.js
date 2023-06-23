d39ade32cea3041c97687e468f7b7d3ae7d6bf41
Patrick R <hi@patrickrein.de>
2023-06-23 09:29:31 +0200

    Removes the document on load callback

--- so-add-badges.js
+++ so-add-badges.js
@@ -1,13 +1,11 @@
 
-//document.addEventListener("DOMContentLoaded", function(){
-	const chatGPTReleaseDate = new Date("2022-11-30");
-	for(element of document.querySelectorAll("span.relativetime")) { 
-		let elementDate = new Date(element.getAttribute("title"));
-		if (elementDate < chatGPTReleaseDate) {
-			element.classList.add("prechatgpt");
-		} else {
-			element.classList.add("postchatgpt");
-		}
+const chatGPTReleaseDate = new Date("2022-11-30");
+for(element of document.querySelectorAll("span.relativetime")) { 
+	let elementDate = new Date(element.getAttribute("title"));
+	if (elementDate < chatGPTReleaseDate) {
+		element.classList.add("prechatgpt");
+	} else {
+		element.classList.add("postchatgpt");
 	}
-	console.log("foo");
-//});
+}
+console.log("foo");
